2656edc1584879bf31cfc2442ee1e6d9790e62e8
Rippsawwall <rippsawwall@gmail.com>
2026-05-03 21:37:45 +0700

    Knockback when collided with enemy

--- Assets/Script/PlayerCollision.cs
+++ Assets/Script/PlayerCollision.cs
@@ -5,7 +5,13 @@ public class PlayerCollision : MonoBehaviour
     public GameObject gameOverUI;
     public GameObject victoryUI;
     public Health playerHealth;
+    private Rigidbody2D rb;
+    public float knockbackForce = 10f;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
@@ -19,6 +25,9 @@ public class PlayerCollision : MonoBehaviour
         if (collision.CompareTag("Enemy"))
         {
             playerHealth.currentHealth -= 10;
+            Transform enemy;
+            
+            ApplyKnockback(collision.transform);
             
             if (playerHealth.currentHealth <= 0)
             {
@@ -47,5 +56,11 @@ public class PlayerCollision : MonoBehaviour
     {
         victoryUI.SetActive(true);
         Time.timeScale = 0f;
+    }
+    
+    void ApplyKnockback(Transform enemy)
+    {
+        Vector2 direction = (transform.position - enemy.position).normalized;
+        rb.AddForce(direction * knockbackForce, ForceMode2D.Impulse);
     }
 }

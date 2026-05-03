08349626daa2ffebc296c39b95744119cce01270
Rippsawwall <rippsawwall@gmail.com>
2026-05-03 22:07:23 +0700

    Level 1 Restore to main

--- Assets/Script/PlayerCollision.cs
+++ Assets/Script/PlayerCollision.cs
@@ -5,13 +5,7 @@ public class PlayerCollision : MonoBehaviour
     public GameObject gameOverUI;
     public GameObject victoryUI;
     public Health playerHealth;
-    private Rigidbody2D rb;
-    public float knockbackForce = 10f;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
@@ -25,9 +19,6 @@ public class PlayerCollision : MonoBehaviour
         if (collision.CompareTag("Enemy"))
         {
             playerHealth.currentHealth -= 10;
-            Transform enemy;
-            
-            ApplyKnockback(collision.transform);
             
             if (playerHealth.currentHealth <= 0)
             {
@@ -56,11 +47,5 @@ public class PlayerCollision : MonoBehaviour
     {
         victoryUI.SetActive(true);
         Time.timeScale = 0f;
-    }
-    
-    void ApplyKnockback(Transform enemy)
-    {
-        Vector2 direction = (transform.position - enemy.position).normalized;
-        rb.AddForce(direction * knockbackForce, ForceMode2D.Impulse);
     }
 }
